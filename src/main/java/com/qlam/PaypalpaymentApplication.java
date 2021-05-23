b6851db343d9aff96f74a45e0467c38fcff0ac30
sonlv1905 <80377041+sonlv1905@users.noreply.github.com>
2021-05-23 21:07:39 +0700

    second commit

---
+++ src/main/java/com/qlam/PaypalpaymentApplication.java
@@ -0,0 +1,13 @@
+package com.qlam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaypalpaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaypalpaymentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qlam/PaypalpaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qlam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaypalpaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
